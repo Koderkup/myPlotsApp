35db81fa2ff79ea7b43fe51545f6872cb8f0141c
Koderkup <koderkup@gmail.com>
2026-03-18 11:23:37 +0300

    remove keys

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -1,17 +1,3 @@
-// import { initializeApp } from 'firebase/app';
-// import { getFirestore } from 'firebase/firestore';
-
-// export const firebaseConfig = {
-//   apiKey: 'AIzaSyCmN8dxWdYnqmcVA17jLINw_LUaz771xYs',
-//   authDomain: 'myplotsapp-295a7.firebaseapp.com',
-//   projectId: 'myplotsapp-295a7',
-//   storageBucket: 'myplotsapp-295a7.firebasestorage.app',
-//   messagingSenderId: '202860526913',
-//   appId: '1:202860526913:web:d74ea34e45c0856fce75c0',
-// };
-
-// const app = initializeApp(firebaseConfig);
-// export const db = getFirestore(app);
 
 import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
